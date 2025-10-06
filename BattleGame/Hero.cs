45039f5bf227f0e9a0af1b042a3eb1459e16dad9
Gonçalo Amaro <samaro.dev@gmail.com>
2025-10-06 11:47:33 +0100

    Upload BattleGame
    Added BattleGame: a logic-driven battle simulation written in C#

---
+++ BattleGame/Hero.cs
@@ -0,0 +1,10 @@
+public class Hero
+{
+    public string Name;
+    public int HP;
+    public Hero(string name, int hp)
+    {
+        Name = name;
+        HP = hp;
+    }
+}
---
+++ BattleGame/Monster.cs
@@ -0,0 +1,10 @@
+public class Monster
+{
+    public string Name;
+    public int HP;
+    public Monster(string name, int hp)
+    {
+        Name = name;
+        HP = hp;
+    }
+}
